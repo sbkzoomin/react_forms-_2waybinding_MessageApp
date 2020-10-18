e09678d28ec88c51e12ebbb78d22864419bc604d
sbkzoomin <bharathsriram5@gmail.com>
2020-10-18 20:02:14 +0530

    2 files modified

--- src/Register.js
+++ src/Register.js
@@ -3,43 +3,82 @@ import React from "react";
 class Register extends React.Component {
   constructor() {
     super();
+    this.state = {
+      username: "Bharat",
+      email: "",
+      password: "",
+      designation: "",
+      terms: false
+    };
   }
+
+  updateInput = event => {
+    if (event.target.type === "checkbox") {
+      this.setState({
+        [event.target.name]: event.target.checked
+      });
+    } else {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+  };
+
+  submitForm = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
     return (
       <React.Fragment>
+        <pre>{JSON.stringify(this.state)}</pre>
         <div className="container mt-3">
           <div className="row">
             <div className="col-md-6">
-              <div className="card">
+              <div className="card bg-light">
                 <div className="card-header bg-danger text-white">
                   <p className="h4">Register Here</p>
                 </div>
                 <div className="card-body">
-                  <form>
+                  <form onSubmit={this.submitForm}>
                     <div className="form-group">
                       <input
                         type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.updateInput}
                         className="form-control"
                         placeholder="username"
                       />
                     </div>
                     <div className="form-group">
                       <input
-                        type="text"
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.updateInput}
                         className="form-control"
                         placeholder="email"
                       />
                     </div>
                     <div className="form-group">
                       <input
-                        type="text"
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.updateInput}
                         className="form-control"
                         placeholder="password"
                       />
                     </div>
                     <div className="form-group">
-                      <select className="form-control">
+                      <select
+                        className="form-control"
+                        name="designation"
+                        value={this.state.designation}
+                        onChange={this.updateInput}
+                      >
                         <option value="Software Engineer">
                           Software Engineer
                         </option>
@@ -51,7 +90,9 @@ class Register extends React.Component {
                       <input
                         className="form-check-input"
                         type="checkbox"
-                        value=""
+                        onChange={this.updateInput}
+                        checked={this.state.terms}
+                        name="terms"
                         id="defaultCheck1"
                       />
                       <label className="form-check-label" for="defaultCheck1">
